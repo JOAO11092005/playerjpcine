6298cd57519660f733b4b96d2fbd98cc65040206
João Paulo Neves Batista <joaopaulonevesbatista@gmail.com>
2024-10-30 23:45:58 -0300

    first commit

--- pages/api/getVideoLink.js
+++ pages/api/getVideoLink.js
@@ -1,16 +1,23 @@
 const axios = require('axios');
-const { response } = require('express');
 
 module.exports = async (req, res) => {
     try {
+        // Tenta fazer uma requisição ao link original
         const response = await axios.get('http://s3-server.net:80/series/876056/Pb9SYJ/91664.mp4', {
-            maxRedirects: 0,
+            maxRedirects: 0, // Para capturar o redirecionamento
             validateStatus: status => status >= 200 && status < 400
         });
-        res.status(200).json({ videoUrl: response.headers.location });
+
+        // Checa se o cabeçalho `location` está presente (indicando redirecionamento)
+        if (response.headers.location) {
+            // Envia o link redirecionado como JSON
+            res.status(200).json({ videoUrl: response.headers.location });
+        } else {
+            // Se não houver redirecionamento, usa o URL original (ou adapte conforme necessário)
+            res.status(200).json({ videoUrl: 'http://s3-server.net:80/series/876056/Pb9SYJ/91664.mp4' });
+        }
     } catch (error) {
         console.error("Erro ao obter o link do vídeo:", error);
         res.status(500).json({ error: 'Não foi possível obter o link do vídeo' });
     }
 };
-console.log(response);
